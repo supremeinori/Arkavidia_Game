e22abf57520225b78d50da5024cb9ab5f6ce81d9
Inori Kay <158485671+supremeinori@users.noreply.github.com>
2026-02-06 12:00:22 +0700

    oke udah ada game managernya. dan udah bisa fix. tinggal di rapihin kalau masih ada bug

--- Arkavidia_Game/Assets/Scenes/cursorlock.cs
+++ Arkavidia_Game/Assets/Scenes/cursorlock.cs
@@ -4,35 +4,45 @@ using UnityEngine.Events;
 
 namespace Unity.Cinemachine.Samples
 {
-    public class CursorLockManager : MonoBehaviour, Unity.Cinemachine.IInputAxisOwner
+    public class CursorLock2 : MonoBehaviour, Unity.Cinemachine.IInputAxisOwner
     {
         public InputAxis CursorLock = InputAxis.DefaultMomentary;
 
-        public UnityEvent OnCursorLocked = new ();
-        public UnityEvent OnCursorUnlocked = new ();
+        public UnityEvent OnCursorLocked = new();
+        public UnityEvent OnCursorUnlocked = new();
 
         bool m_IsTriggered;
+        bool allowToggle = true;
 
         public void GetInputAxes(List<IInputAxisOwner.AxisDescriptor> axes)
         {
             axes.Add(new()
             {
-                DrivenAxis = () => ref CursorLock, Name = "CursorLock",
+                DrivenAxis = () => ref CursorLock,
+                Name = "CursorLock",
                 Hint = IInputAxisOwner.AxisDescriptor.Hints.X
             });
         }
 
         void OnValidate() => CursorLock.Validate();
-        void OnEnable() => UnlockCursor();
-        void OnDisable() => UnlockCursor();
+
+        void Start()
+        {
+            // Gameplay start = locked
+            LockCursor();
+        }
 
         void Update()
         {
+            if (!allowToggle)
+                return;
+
             if (CursorLock.Value == 0)
                 m_IsTriggered = false;
             else if (!m_IsTriggered)
             {
                 m_IsTriggered = true;
+
                 if (Cursor.lockState == CursorLockMode.None)
                     LockCursor();
                 else
@@ -40,18 +50,33 @@ namespace Unity.Cinemachine.Samples
             }
         }
 
+        // ==========================
+        // CALLED BY GAME MANAGER
+        // ==========================
+
+        public void ForceLock()
+        {
+            allowToggle = true;
+            LockCursor();
+        }
+
+        public void ForceUnlock()
+        {
+            allowToggle = false;
+            UnlockCursor();
+        }
+
         public void LockCursor()
         {
-            if (enabled)
-            {
-                Cursor.lockState = CursorLockMode.Locked;
-                OnCursorLocked.Invoke();
-            }
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            OnCursorLocked.Invoke();
         }
 
         public void UnlockCursor()
         {
             Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
             OnCursorUnlocked.Invoke();
         }
     }
--- Arkavidia_Game/Assets/Scenes/pausepanel.cs
+++ Arkavidia_Game/Assets/Scenes/pausepanel.cs
@@ -1,13 +1,13 @@
-using UnityEngine;
+// using UnityEngine;
 
-public class PauseUIRegister : MonoBehaviour
-{
-    void Awake()
-    {
-        if (GameManager.Instance != null)
-        {
-            GameManager.Instance.pausePanel = gameObject;
-            gameObject.SetActive(false);
-        }
-    }
-}
+// public class PauseUIRegister : MonoBehaviour
+// {
+//     void Awake()
+//     {
+//         if (GameManager.Instance != null)
+//         {
+//             GameManager.Instance.pausePanel = gameObject;
+//             gameObject.SetActive(false);
+//         }
+//     }
+// }
